4756c232c1ee9e07e9605751764eb4b6d389e46c
Eduarda Vieira <eduardavieiranetto@gmail.com>
2019-04-27 21:36:04 -0300

    Curso 01

---
+++ Curso Introdução/script.js
@@ -0,0 +1,48 @@
+//AULAS INTRODUTÓRIAS//
+
+/*** Aula 01 - Hello World ***/
+    //alert("Olá mundo");
+    //alert gera um popup de mensagem do próprio browser
+
+
+/*** Aula 02 - Variáveis ***/
+    //var mensagem = "Olá mundo"
+    //alert(mensagem);
+
+
+/*** Aula 03 - Operações Matemáticas ***/
+    /*var a = 2;
+    var b = 3;
+    var soma = a+b;
+    var sub = a-b;
+    alert(soma);
+    alert(sub);*/
+
+
+/*** Aula 04 - Estrutura if-else ***/
+    /*var a = 2;
+    var b = 3;
+    var soma = a+b;
+    var sub = a-b;
+    if(b % 2 == 1) {
+        alert("Número ímpar");
+    } else {
+        alert("Número par");
+    } */
+
+
+/*** Aula 05 - Array ***/  
+    /*var lista = ["laranja", "maçã", "banana"];
+    alert(lista[0]);
+    for(i in lista) {
+        alert(i); //irá mostrar todos os indices da lista, de 0 a 2
+        alert(lista[i]); // irá mostrar o conteudo da lista
+    } */
+
+
+/*** Aula 06 - Console ***/ 
+    //console.log("olá mundo");
+
+
+/*** Aula 07 - Funções ***/ 
+    
